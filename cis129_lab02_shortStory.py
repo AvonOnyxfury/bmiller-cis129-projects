ca584d69586dd5849a0e9a6cdd142326bdce5054
Bryce Miller <159302740+AvonOnyxfury@users.noreply.github.com>
2024-02-09 22:11:18 -0700

    Update cis129_lab02_shortStory.py
    Replaces "print('Hello, World!')" with a new set of print statements which displays a story.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+#Create a short story out of 4-5 print statements to replace the display of "Hello World in Main."
+#Author: Bryce Miller
+#My first project for cis129.
+#Prints out a short sory about the war machine planet. 
+print('Long ago, the world was covered in a great machine ruled by a single computer, ignorant of the humans surviving within it.')
+print('One day, the computer spontaniously failed, throwing this metal world into chaos and dissarray and threatening the surviving cities.')
+print('A century later, humans, who longed for peace, decided they would journey to their mechanical ruler, attempting to revive him.')
+print("But, by the time they reached him, they learned of the world that existed before the great machine and of the machine's warlike purpose.")
+print('They decided instead that they would let the machine rot and crumble so that the world could return to its natural state.') 
